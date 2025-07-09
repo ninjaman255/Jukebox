ae264b30999a3f63cf72f07c40ceb4dfd93d7319
Ninjaman255 <aaronclevette@gmail.com>
2025-07-09 08:19:54 -0500

    added in indianas updates/fixes to jukebox script and map

--- scripts/jukebox.lua
+++ scripts/jukebox.lua
@@ -1,7 +1,14 @@
--- finalized table
-local Songs = {}
+--[[
+* ---------------------------------------------------------- *
+                 Jukebox v2.1 by D3str0y3d
+	 https://github.com/ninjaman255/Server_12_22_2021_Jukebox
+* ---------------------------------------------------------- *
+]]--
 
---Color for BBS
+print("[jukebox] Loading the groove!")
+
+-- defaults
+local Songs = {}
 local color = 
   {
   r = 0,
@@ -9,29 +16,68 @@ local color =
   b = 0
   }
 
-function listFiles(directory)
-  -- make a table to collect the file names
-  local songTable = {}    
-  -- handles running the cmd
-  local cmd = 'ls "' .. directory .. '"'
-  local handle = io.popen(cmd)
+--Shorthand for async
+function async(p)
+    local co = coroutine.create(p)
+    return Async.promisify(co)
+end
 
-  if handle then
-      for file in handle:lines() do
-          table.insert(songTable, file)
-      end
-      handle:close()
-      print(tostring(songTable))
+--Shorthand for await
+function await(v) return Async.await(v) end
+
+--purpose: splits a string based on a delimiter
+local function splitter(inputstr, sep)
+    if sep == nil then
+        sep = '%s'
+    else
+        sep = sep:gsub("[%(%)%.%%%+%-%*%?%[%]%^%$]", "%%%1")
+    end
+    
+    local t = {}
+    for str in (inputstr..sep):gmatch("(.-)"..sep) do
+        table.insert(t, str)
+    end
+    return t
+end
+
+--purpose: checks if server is running on windows or unix and adjusts populating the song list accordingly
+local function get_os()
+  if package.config:sub(1,1) == "\\" then
+    return "windows"
   else
-      print("Error listing files in directory.")
+    return "unix"
   end
-  return songTable
+end
+  
+local function listFiles(directory)
+    local os_type = get_os()
+    print("[jukebox] Populating song list from "..os_type.." system.")
+    -- make a table to collect the file names
+    local songTable = {}    
+    -- handles running the cmd
+    local cmd = 'ls "' .. directory .. '"'
+    if os_type == "windows" then 
+      local cmd = 'dir "' .. directory .. '" /b'
+    end 
+
+    local handle = io.popen(cmd)
+
+    if handle then
+        for file in handle:lines() do
+            table.insert(songTable, file)
+        end
+        handle:close()
+    else
+        print("Error listing files in directory.")
+    end
+    return songTable
+    
 end
 
 -- grabbing initial song names from dir "server/assets/jukebox"
 local songList = listFiles("./assets/jukebox")
 
-function CreatePost(i, name, author) 
+local function CreatePost(i, name, author) 
   return {
   id = i,
   title = string.gsub(name, "%.ogg$", ""),
@@ -41,64 +87,59 @@ function CreatePost(i, name, author)
 end
 
 -- Creates posts with file name 
-function CompilePosts(inputTable, author, finalizedTable)
+local function CompilePosts(inputTable, author, finalizedTable)
 for key, value in pairs(inputTable) do
-  print(key, value)
   local postToAdd = CreatePost(key, value, author or "")
   table.insert(finalizedTable, postToAdd)
 end
-print(finalizedTable)
-table.insert(finalizedTable, CreatePost(#finalizedTable+1, "Exit", ""))
-print(tostring(finalizedTable))
+table.insert(finalizedTable, CreatePost(#finalizedTable+1, "Close Jukebox", ""))
 end
 
 -- run the function
 CompilePosts(songList, "", Songs)
 
-function handle_post_selection(player_id,post_id) 
-  local numberfiedPostID = tonumber(post_id) 
-  if numberfiedPostID == #Songs then
-    Async.question_player(player_id,("Do you wish to close the Jukebox?"))
-    .and_then(function(response)
-    if response == 0 then
-      print("they said no")
-    end
-    if (response == 1) then
-      Net.close_bbs(player_id)  
+Net:on("post_selection", function(event)
+  return async(function ()
+    local numberfiedPostID = tonumber(event.post_id) 
+    if numberfiedPostID == #Songs then
+        Net.close_bbs(event.player_id)  
+    elseif numberfiedPostID <= #Songs then
+      response = await(Async.question_player(event.player_id,("Do you wish to change the song to "..Songs[tonumber(event.post_id)].title.."?")))
+      if (response == 1) then
+          local area_id = Net.get_player_area(event.player_id)
+          Net.set_song(area_id, "/server/assets/jukebox/"..Songs[tonumber(event.post_id)].title..".ogg")
+          Net.close_bbs(event.player_id)
+        end
     end
   end)
-  elseif numberfiedPostID <= #Songs then
-    Async.question_player(player_id,("Do you wish to change the song to "..Songs[tonumber(post_id)].title.."?"))
-    .and_then(function(response)
-    if response == 0 then
-      print("they said no")
-    end
-    if (response == 1) then
-        print("Attempting to set song")
-        local area_id = Net.get_player_area(player_id)
-        Net.set_song(area_id, "/server/assets/jukebox/"..Songs[tonumber(post_id)].title..".ogg")
-        print("successfully set song")
-        Net.close_bbs(player_id)
-        print("Closed BBS with success")
-        print(post_id)
-      end
-    end)
-  end
-end
+end)
 
+Net:on("object_interaction", function(event)
+    local area_id = Net.get_player_area(event.player_id)
+    local object = Net.get_object_by_id(area_id, event.object_id)
 
-     function handle_object_interaction(player_id, object_id, button)
-      local area_id = Net.get_player_area(player_id)
-      local object = Net.get_object_by_id(area_id, object_id)
-      if object.custom_properties.Jukebox ~= nil then
-      print("Checked Custom properties to make sure its Jukebox")
-      if object.custom_properties.Color ~= nil then
-        color.r = object.custom_properties.r
-        color.g = object.custom_properties.g
-        color.b = object.custom_properties.b
-        Net.open_board(player_id, "Songs", color, Songs)
-      end
-      elseif object.custom_properties.Color == nil then
-        Net.open_board(player_id, "Songs", color, Songs)
+    --if we didn't interact with a jukebox then stop
+    if object.class ~= "Jukebox" and object.type ~= "Jukebox" then
+      return
+    end 
+
+    --if we didn't interact with A/Interact button then stop
+    if event.button ~= 0 then 
+      return
+    end 
+    
+    if object.custom_properties.Color ~= nil then
+      --print("[jukebox] Loaded custom color for jukebox")
+      color_parts = splitter(object.custom_properties.Color,",")
+      if color_parts[3] ~= nil then
+        color.r = color_parts[1]
+        color.g = color_parts[2]
+        color.b = color_parts[3]
+      else
+        print("[jukebox] The color value of '"..object.custom_properties.Color.."' is malformed.")
       end
     end
+    Net.open_board(event.player_id, "Songs", color, Songs)
+
+end)
+
